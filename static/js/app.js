3d4c033567889b069fb6490ddc0a8daf813ee5ab
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2023-03-11 16:31:54 +0300

    first commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   const ctx = document.getElementById("myChart").getContext("2d");
+  var sensorValue = 0
 
   const myChart = new Chart(ctx, {
     type: "line",
@@ -7,12 +8,32 @@ $(document).ready(function () {
       datasets: [{ label: "Sensor 1", pointRadius: 0, }],
     },
     options: {
+      animation: false,
       borderWidth: 1,
       borderColor: ['rgba(255, 99, 132, 1)',],
       scales: {
         xAxis: {
-            display: false,
-        }
+          display: false,
+        },
+        yAxis: {
+          display: true,
+          max: (scale) => (
+            scale.chart.data.datasets.reduce((acc, curr) => {
+              var max = 0
+              if (Array.isArray(curr.data)) {
+                max = Math.max(...curr.data) + 20000;
+              }
+              return max;
+            }, Number.MIN_SAFE_INTEGER)),
+          min: (scale) => (
+            scale.chart.data.datasets.reduce((acc, curr) => {
+              var min = 0
+              if (Array.isArray(curr.data)) {
+                min = Math.min(...curr.data) - 20000;
+              }
+              return min;
+            }, Number.MIN_SAFE_INTEGER))
+      }
       }
     },
   });
@@ -35,7 +56,8 @@ $(document).ready(function () {
   const MAX_DATA_COUNT = 600;
   const socket = new WebSocket('ws://' + location.host + '/ws');
   socket.addEventListener('message', msg => {
-    const data = JSON.parse(msg.data);
+    const data  = JSON.parse(msg.data);
+    sensorValue = data.value;
     console.log("Received sensorData :: " + data.value);
 
     if (myChart.data.labels.length > MAX_DATA_COUNT) {
